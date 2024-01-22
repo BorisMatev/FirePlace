01f8bca002f380b3353d5f41dc9e1906f0248153
BorisMatev <boris.matev72345@gmail.com>
2024-01-22 23:53:59 +0200

    Add auth guard and home component

--- FirePlace/ClientApp/src/app/app.routes.ts
+++ FirePlace/ClientApp/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'log-in', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'home', canActivate:[authGuard], component: HomeComponent },
     { path: '',   redirectTo: 'log-in', pathMatch: 'full' }, // redirect to 'log-in'
     { path: '**', component: LoginComponent },  // Wildcard route for a 404 page
   ];
