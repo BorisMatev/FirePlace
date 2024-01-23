41709e3ef59b7a9db0c5444f6e41cff3ad58c096
BorisMatev <106867539+BorisMatev@users.noreply.github.com>
2024-01-23 11:34:07 +0200

    Add components and update auth guard

--- FirePlace/ClientApp/src/app/core/guards/auth.guard.ts
+++ FirePlace/ClientApp/src/app/core/guards/auth.guard.ts
@@ -1,10 +1,13 @@
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const token = localStorage.getItem("token");
+  const router = inject(Router);
   if (token) {
     return true;
   } else{
+    router.navigate(['log-in']);
     return false;
   }
 };
