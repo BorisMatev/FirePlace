e6b246d415f9da9a9fbacda7fff098f3fb80f8b3
BorisMatev <boris.matev72345@gmail.com>
2024-02-01 23:28:41 +0200

    Welcome page

--- FirePlace/ClientApp/src/app/welcome-page/welcome-page.component.ts
+++ FirePlace/ClientApp/src/app/welcome-page/welcome-page.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [GalleriaModule],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss'
 })
 export class WelcomePageComponent {
-
+  images = [
+    "../../assets/gallery/bench.jpeg",
+    "../../assets/gallery/sky.jpg",
+    "../../assets/gallery/waterfall.jpg"
+  ];
 }
