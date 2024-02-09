9e4f3d0f2e7f156e9929c6c08b808dde9655424d
BorisMatev <boris.matev72345@gmail.com>
2024-02-09 23:14:00 +0200

    Make animation on scroll

--- FirePlace/ClientApp/src/app/welcome-page/welcome-page.component.ts
+++ FirePlace/ClientApp/src/app/welcome-page/welcome-page.component.ts
@@ -1,34 +1,52 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, inject } from '@angular/core';
 import { GalleriaModule } from 'primeng/galleria';
 
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [GalleriaModule,AnimateOnScrollModule],
+  imports: [GalleriaModule, AnimateOnScrollModule],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
-  animations:[
-    trigger("showOnScroll",[
-      state("first",style({
+  animations: [
+    trigger("showOnScroll", [
+      state("first", style({
         opacity: 0,
-        transform: 'translateX(200px)',
+        transform: 'translateX(100px)',
       })),
-      state("second",style({
+      state("second", style({
         opacity: 1,
         transform: 'translateX(0px)',
       })),
-      transition('first => second', animate('2s')),
+      transition('first => second', animate('1s')),
     ])
   ]
 })
 export class WelcomePageComponent {
   shown = "first";
 
-  asd(){
+  animation() {
     this.shown = "second"
   }
+
+  @ViewChild('test') testElement!: ElementRef;
+
+  isElementInViewport(element: HTMLElement): boolean {
+    const rect = element.getBoundingClientRect();
+    return element.getBoundingClientRect().top <= 400;
+  }
+
+
+  @HostListener('document:mousewheel')
+  onScroll(): void {
+    const element = this.testElement.nativeElement;
+    if (this.isElementInViewport(element)) {
+      this.animation();
+    }
+  }
+
 
   images = [
     "../../assets/gallery/bench.jpeg",
